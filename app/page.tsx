4056af42cd547259a96ec7426e183d0c57a301f2
Aspect <32452006+aspectdev-ph@users.noreply.github.com>
2023-01-04 08:31:01 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Page({ information }: any) {
       <>
         <div className="mockup-window border w-screen h-screen bg-base-300">
           <div className="flex justify-center w-full h-full px-4 py-16 bg-base-200">
-            <p className="text-center my-auto text-2xl animate-ping">
+            <p className="text-center my-auto text-2xl animate-bounce shadow-2xl animate-ping">
               {" "}
               Please wait...
             </p>
