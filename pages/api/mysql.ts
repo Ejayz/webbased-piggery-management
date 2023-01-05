57c4e838994984fe5caf8bdcd2f875f5c08ad6d7
Aspect <32452006+aspectdev-ph@users.noreply.github.com>
2023-01-06 00:40:36 +0800

    Update mysql.ts

--- pages/api/mysql.ts
+++ pages/api/mysql.ts
@@ -1,11 +1,11 @@
-import * as mysql from "mysql"
-import * as dotenv from "dotenv"
-dotenv.config()
-var connection =mysql.createPool({
-    host     : process.env.HOST,
-    user     : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE
-
-})
+import * as mysql from "mysql";
+import * as dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
+var connection = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
 export default connection;
