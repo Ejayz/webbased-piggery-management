365bec39b177e761335d0f30a910185f924f9793
Aspect <32452006+aspectdev-ph@users.noreply.github.com>
2023-01-06 22:41:04 +0800

    Added hasura

--- pages/api/post/login.tsx
+++ pages/api/post/login.tsx
@@ -3,13 +3,11 @@ import * as dotenv from "dotenv";
 import connection from "../mysql";
 import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { send } from "micro/types/src/lib";
 dotenv.config();
+const secret: any = process.env.HASURA_SECRET;
 const jwt_key: any = process.env.JWT_KEY;
 //API Function that only accepts post request
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  bcrypt.genSalt(15).then((salt) => {});
-
   if (req.method !== "POST") {
     res.status(405).json({
       code: 405,
@@ -18,9 +16,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return 0;
   }
   const { username, password, rememberme } = req.body;
-  const query = "select * from tbl_user where IS_EXIST='true' and USERNAME=?";
-  VerifyUser(username, password, query)
+  VerifyUser(username)
     .then((result: any) => {
+      console.log(result);
       const data = result[0];
       bcrypt.compare(password, data.password, (err, result) => {
         if (result) {
@@ -64,16 +62,26 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     });
 }
 
-async function VerifyUser(username: any, password: any, query: any) {
-  return new Promise((resolve, rejects) => {
-    connection.getConnection((err, conn) => {
-      conn.query(query, [username], (err, result, feild) => {
-        if (err) {
-          rejects(err);
-        }
-        resolve(result);
-      });
-      conn.release();
-    });
+async function VerifyUser(username: string) {
+  //API function to request in hasura
+  console.log(secret);
+  let headersList: Record<string, string> = {
+    Accept: "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "x-hasura-admin-secret": secret,
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    username: username,
   });
+
+  let response = await fetch("http://localhost:8080/api/rest/login", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.text();
+  return JSON.parse(data);
 }
