c8f734d49fb154ff1cb61d2d7cbac80b26e883cc
Ejayz <aj1234567809@gmail.com>
2023-02-06 21:40:43 +0800

    Updated Keywords

--- pages/api/post/UserManagement/SearchUser/[keyword].ts
+++ pages/api/post/UserManagement/SearchUser/[keyword].ts
@@ -2,7 +2,6 @@ import { getCookie } from "cookies-next";
 import { NextApiRequest, NextApiResponse } from "next";
 import { decodeJWT, verifyJWT } from "pages/api/jwtProcessor";
 import connection from "../../../mysql";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -14,7 +13,6 @@ export default async function handler(
       message: "401 Authorization Error. Please login first.",
     });
   }
-
   const verify = await verifyJWT(cookie);
   if (!verify) {
     return res.status(401).json({
@@ -22,12 +20,10 @@ export default async function handler(
       message: "401 Invalid Session. Please relogin.",
     });
   }
-
   const decode: any = await decodeJWT(cookie);
   const user_id = decode.user_id;
   const { keyword } = req.query;
   const data: any = await SearchUser({ keyword, user_id });
-  console.log(data);
   if (data.length >= 1) {
     return res.status(200).json({ code: 200, data: data });
   } else {
