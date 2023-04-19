9a5b6ee5d6b8e8b7b1da1aa063d701fa1d01e635
Ejayz <aj1234567809@gmail.com>
2023-04-19 17:24:48 +0800

    Update backup.ts

--- pages/api/post/backup.ts
+++ pages/api/post/backup.ts
@@ -49,12 +49,10 @@ export default async function handler(
 async function UpdateCage() {
   const result = await mysqldump({
     connection: {
-      host: process.env.DB_HOST ? process.env.DB_HOST : "localhost",
-      user: process.env.DB_USER ? process.env.DB_USER : "root",
-      password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : "randomDdos1.com",
-      database: process.env.DB_DATABASE
-        ? process.env.DB_DATABASE
-        : "piggery_management",
+      host: "localhost",
+      user: "root",
+      password: "randomDdos1.com",
+      database: "piggery_management",
     },
     dump: { schema: { table: { dropIfExist: true } } },
   });
