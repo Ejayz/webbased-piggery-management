34d25171e90fa09d9f93ad5d6e4c1abd466fd7e9
Ejayz <aj1234567809@gmail.com>
2023-04-22 13:20:26 +0800

    Updates

--- hooks/getErrors.ts
+++ hooks/getErrors.ts
@@ -1,5 +1,5 @@
 export default function ErrorMessage(errors: any, form_name: string) {
-  console.log(errors[form_name]);
+
   if (Object.keys(errors).length !== 0) {
     return Object.keys(errors[form_name]);
   } else {
